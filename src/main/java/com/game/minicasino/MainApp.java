f06a05ecbbe9c417244aae96a9b7a0e9a66882a0
RobertR-01 <robert.rupik@gmail.com>
2022-07-29 11:43:32 +0200

    Add handlers for About and Exit
    Added some basic controls to the main app window (FXML).
    Added event handlers for About and Exit buttons.
    Modified some main app window visible properties (title etc.).
    Added two temporary snippets (dialog templates) for further use.
    Added version field to the MainApp class.

--- src/main/java/com/game/minicasino/MainApp.java
+++ src/main/java/com/game/minicasino/MainApp.java
@@ -7,12 +7,14 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class MainApp extends Application {
+    public final static String APP_VERSION = "0.0.1";
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("main-window.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 800, 500);
+        stage.setTitle("MiniCasino \u00A9");
         stage.setScene(scene);
         stage.show();
     }
---
+++ src/main/java/com/game/minicasino/MainAppController.java
@@ -0,0 +1,43 @@
+package com.game.minicasino;
+
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+import javafx.scene.text.Font;
+
+public class MainAppController {
+    @FXML
+    private Label titleLabel;
+
+    public void initialize() {
+        // title label
+        titleLabel.setText("Mini Casino\nThe Game");
+        titleLabel.setFont(Font.font("Algerian", 30));
+
+    }
+
+    @FXML
+    public void handlePlayButton() {
+
+    }
+
+    @FXML
+    public void handleOptionsButton() {
+
+    }
+
+    @FXML
+    public void handleAboutButton() {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("About Mini Casino");
+        alert.setHeaderText("Mini Casino: The Game");
+        alert.setContentText("v" + MainApp.APP_VERSION + "\n\nCopyright \u00A9 2022 Mr Smalec");
+        alert.show();
+    }
+
+    @FXML
+    public void handleExitButton() {
+        Platform.exit();
+    }
+}
