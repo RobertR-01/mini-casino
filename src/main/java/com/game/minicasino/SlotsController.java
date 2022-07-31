64ddcad31ef0fd5e5b692ceee2efd7d8c5ffe6fd
RobertR-01 <robert.rupik@gmail.com>
2022-07-31 18:29:55 +0200

    Implement reel spinning mechanics
    Implemented initial version of the reel-spinning mechanics in
    SLotsController. This approach is based on a single, static version of
    the symbols' list, while labels on the reel load consecutive symbols
    via 3 separate iterations.
    Added some required styles to styles.css for the purpose of the
    SPIN/STOP button swapping.

--- src/main/java/com/game/minicasino/SlotsController.java
+++ src/main/java/com/game/minicasino/SlotsController.java
@@ -1,17 +1,22 @@
 package com.game.minicasino;
 
 import com.game.logic.Slots;
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Region;
 import javafx.scene.text.Font;
 
 import java.io.IOException;
+import java.util.List;
 
 public class SlotsController {
     @FXML
@@ -28,17 +33,133 @@ public class SlotsController {
     private Label firstReelPositionTwo;
     @FXML
     private Label firstReelPositionThree;
+    @FXML
+    private Button spinButton;
 
     private ObservableList<Slots.SlotSymbol> observableFirstReel;
+    private List<Slots.SlotSymbol> symbolsList;
+    private ImageView firstReelPositionOneImageView;
+    private ImageView firstReelPositionTwoImageView;
+    private ImageView firstReelPositionThreeImageView;
+    private int globalCounter;
+    int firstPositionCounter;
+    int secondPositionCounter;
+    int thirdPositionCounter;
 
     @FXML
     public void initialize() {
         // title label setup
         titleLabel.setFont(Font.font("Times New Roman", 20));
+        spinButton.idProperty().set("spinButtonStyle");
+        spinButton.fontProperty().set(new Font("Arial Bold", 20.0));
 
         observableFirstReel = FXCollections.observableArrayList(Slots.getSlotsInstance().getSymbolsList());
 
+        // initializing reels
+        symbolsList = Slots.getSlotsInstance().getSymbolsList();
+        firstReelPositionOneImageView = (ImageView) firstReelPositionOne.getGraphic();
+        firstReelPositionTwoImageView = (ImageView) firstReelPositionTwo.getGraphic();
+        firstReelPositionThreeImageView = (ImageView) firstReelPositionThree.getGraphic();
+        globalCounter = 0;
+        firstPositionCounter = 0;
+        secondPositionCounter = 1;
+        thirdPositionCounter = 2;
 
+        // initial symbols on reels
+        firstReelPositionThreeImageView.setImage(symbolsList.get(0).getImage());
+        firstReelPositionTwoImageView.setImage(symbolsList.get(1).getImage());
+        firstReelPositionOneImageView.setImage(symbolsList.get(2).getImage());
+    }
+
+    @FXML
+    public void handleSpinButton() {
+        System.out.println("handleSpinButton -> globalCounter = " + globalCounter);
+        spinButton.setText("STOP");
+        spinButton.idProperty().set("stopButtonStyle");
+        spinButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                handleStopButton();
+            }
+        });
+
+        Runnable spinOneSymbol = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    globalCounter = 0;
+                    while (true) {
+                        System.out.println("handleSpinButton -> while() -> globalCounter = " + globalCounter);
+                        // fix code duplication
+                        if (globalCounter >= 50) {
+                            break;
+                        }
+                        for (int i = 0; i < symbolsList.size(); i++) {
+                            System.out.println("handleSpinButton -> for() -> globalCounter = " + globalCounter);
+                            // fix code duplication
+                            if (globalCounter >= 50) {
+                                break;
+                            }
+//                            System.out.println("First counter: " + firstPositionCounter);
+                            firstReelPositionThreeImageView.setImage(symbolsList.get(firstPositionCounter).getImage());
+                            firstPositionCounter++;
+                            if (firstPositionCounter > 9) {
+                                firstPositionCounter = 0;
+                            }
+
+//                            System.out.println("Second counter: " + secondPositionCounter);
+                            firstReelPositionTwoImageView.setImage(symbolsList.get(secondPositionCounter).getImage());
+                            secondPositionCounter++;
+                            if (secondPositionCounter > 9) {
+                                secondPositionCounter = 0;
+                            }
+
+//                            System.out.println("Third counter: " + thirdPositionCounter);
+                            firstReelPositionOneImageView.setImage(symbolsList.get(thirdPositionCounter).getImage());
+                            thirdPositionCounter++;
+                            if (thirdPositionCounter > 9) {
+                                thirdPositionCounter = 0;
+                            }
+
+                            Thread.sleep(100);
+                            globalCounter++;
+                        }
+                    }
+                    Platform.runLater(new Runnable() {
+                        @Override
+                        public void run() {
+                            stopSpinning();
+                        }
+                    });
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        new Thread(spinOneSymbol).start();
+    }
+
+    private void startSpinning() {
+
+    }
+
+    private void stopSpinning() {
+        spinButton.setText("SPIN");
+        spinButton.idProperty().set("spinButtonStyle");
+        spinButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                handleSpinButton();
+            }
+        });
+        System.out.println("\thandleStopButton -> globalCounter = " + globalCounter);
+    }
+
+    @FXML
+    public void handleStopButton() {
+        globalCounter = 50;
+        stopSpinning();
     }
 
     @FXML
