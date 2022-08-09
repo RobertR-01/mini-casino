fc5225dd2468897110204c9b435e18a00e5eb0de
RobertR-01 <robert.rupik@gmail.com>
2022-08-09 13:51:40 +0200

    Fix active profile selection
    Fixed both UI and data aspect of selecting the active profile. The app
    now remembers which profile should be active, and the corresponding row
    in the window for profile choice is now selected properly every time
    it's brought up. The ProfileData list of profiles is updated
    accordingly to the changes made in the UI.

--- src/main/java/com/minicasino/ui/ProfileChoiceController.java
+++ src/main/java/com/minicasino/ui/ProfileChoiceController.java
@@ -13,12 +13,10 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.text.Font;
 import javafx.stage.Modality;
+import org.jdom2.Element;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 public class ProfileChoiceController {
     @FXML
@@ -29,6 +27,14 @@ public class ProfileChoiceController {
     private Button backButton;
     @FXML
     private HBox profile0HBox;
+    @FXML
+    private HBox profile1HBox;
+    @FXML
+    private HBox profile2HBox;
+    @FXML
+    private HBox profile3HBox;
+    @FXML
+    private HBox profile4HBox;
     @FXML
     private ToggleGroup profileToggleGroup;
     @FXML
@@ -44,6 +50,7 @@ public class ProfileChoiceController {
 
     private Toggle previouslySelectedRadioButton;
     private List<Button> buttonList;
+    private Map<Integer, HBox> profileHBoxMap;
 
     @FXML
     public void initialize() {
@@ -58,7 +65,33 @@ public class ProfileChoiceController {
         // header label setup:
         headerLabel.setFont(Font.font("Times New Roman", 20));
 
+        // HBox map for fetching Buttons, RadioButtons etc.:
+        profileHBoxMap = Map.of(0, profile0HBox, 1, profile1HBox, 2, profile2HBox, 3, profile3HBox, 4, profile4HBox);
+
         // data binding
+        for (Map.Entry<Integer, HBox> hBoxEntry : profileHBoxMap.entrySet()) {
+            Button button = (Button) hBoxEntry.getValue().getChildren().get(1);
+            button.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent event) {
+                    handleProfileButton(event);
+                }
+            });
+            int index = hBoxEntry.getKey();
+            button.textProperty().bind(Bindings.createObjectBinding(
+                    () -> ProfileData.getProfileDataInstance().getProfileList().get(index).getName(),
+                    ProfileData.getProfileDataInstance().getProfileList()));
+        }
+
+        // selecting previously set active profile (row):
+        ProfileData.Profile activeProfile = ProfileData.getProfileDataInstance().getActiveProfile();
+        if (activeProfile != null) {
+            int index = ProfileData.getProfileDataInstance().getProfileList().indexOf(activeProfile);
+            profileHBoxMap.get(index).setId("selectedProfileRow");
+            ((Toggle) profileHBoxMap.get(index).getChildren().get(2)).setSelected(true);
+        }
+
+/*        // old
         buttonList = new ArrayList<>();
         Collections.addAll(buttonList, profile0Button, profile1Button, profile2Button, profile3Button, profile4Button);
         for (int i = 0; i < 5; i++) {
@@ -72,7 +105,7 @@ public class ProfileChoiceController {
             buttonList.get(i).textProperty().bind(Bindings.createObjectBinding(
                     () -> ProfileData.getProfileDataInstance().getProfileList().get(index).getName(),
                     ProfileData.getProfileDataInstance().getProfileList()));
-        }
+        }*/
     }
 
     @FXML
@@ -82,22 +115,74 @@ public class ProfileChoiceController {
 
     @FXML
     public void handleRadioButton(ActionEvent event) {
+        // TODO: consider data binding for style property -> isActive of the profile in the profile list
+        // finding event source - selected toggle:
         Toggle sourceRadioButton = (Toggle) event.getSource();
-        ((Node) sourceRadioButton).getParent().setId("selectedProfileRow");
-
-        if (previouslySelectedRadioButton != null) {
-            ((Node) previouslySelectedRadioButton).getParent().setId("unselectedProfileRow");
+        int index = findHBoxMapKeyByChild(profileHBoxMap, (Node) sourceRadioButton);
+        // setting all profiles to inactive first:
+        for (ProfileData.Profile profile : ProfileData.getProfileDataInstance().getProfileList()) {
+            profile.setActive(false);
         }
-        previouslySelectedRadioButton = sourceRadioButton;
+        // unselecting all rows:
+        for (Map.Entry<Integer, HBox> entry : profileHBoxMap.entrySet()) {
+            entry.getValue().setId("unselectedProfileRow");
+        }
+        // selecting the current row:
+        profileHBoxMap.get(index).setId("selectedProfileRow"); // border
+        // setting active profile:
+        ProfileData.getProfileDataInstance().getProfileList().get(index).setActive(true);
 
-        // more
+
+/*
+
+        // old - using current and previously active profile:
+        // TODO: consider data binding for style property -> isActive of the profile in the profile list
+        // selecting the current row:
+        Toggle sourceRadioButton = (Toggle) event.getSource();
+        int index = findHBoxMapKeyByChild(profileHBoxMap, (Node) sourceRadioButton);
+        profileHBoxMap.get(index).setId("selectedProfileRow"); // border
+        // setting active profile:
+        ProfileData.getProfileDataInstance().getProfileList().get(index).setActive(true);
+
+        // skipped if there was no active profile before:
+        if (previouslySelectedRadioButton != null) {
+            int previouslySelectedIndex = findHBoxMapKeyByChild(profileHBoxMap, (Node) previouslySelectedRadioButton);
+            // setting previous profile as inactive:
+            ProfileData.getProfileDataInstance().getProfileList().get(previouslySelectedIndex).setActive(false);
+            // unselecting the previously selected row:
+            ((Node) previouslySelectedRadioButton).getParent().setId("unselectedProfileRow"); // no border
+        }
+        previouslySelectedRadioButton = sourceRadioButton;*/
     }
+
+    private int findHBoxMapKeyByChild(Map<Integer, HBox> map, Node child) {
+        for (Map.Entry<Integer, HBox> entry : map.entrySet()) {
+            for (Node node : entry.getValue().getChildren()) {
+                if (node.equals(child)) {
+                    return entry.getKey();
+                }
+            }
+        }
+        System.out.println("ProfileChoiceController.findHBoxMapKeyByChild() -> no such key");
+        return -1;
+    }
+
+//    private int findMapKeyByButton(Map<Integer, HBox> map, Button button) {
+//        for (Map.Entry<Integer, HBox> entry : map.entrySet()) {
+//            if (entry.getValue().getChildren().get(1).equals(button)) {
+//                return entry.getKey();
+//            }
+//        }
+//        System.out.println("ProfileChoiceController.findMapKeyByButton() -> no such key");
+//        return -1;
+//    }
 
     @FXML
     public void handleProfileButton(ActionEvent event) {
         // set the currently edited profile:
         Button eventSource = (Button) event.getSource();
-        int index = buttonList.indexOf(eventSource);
+        // TODO: check for -1 from findMapKeyByButton
+        int index = findHBoxMapKeyByChild(profileHBoxMap, eventSource);
         ProfileData.Profile editedProfile = ProfileData.getProfileDataInstance().getProfileList().get(index);
         editedProfile.setBeingEdited(true); // TODO: move it to the ProfileData from the Profile class
         // TODO: obsolete step?
